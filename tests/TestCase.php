313670b213ef068573ec15f3ea3944134d1cbab7
Vritika-Lar <v00243082@gmail.com>
2026-05-06 21:04:21 +0530

    Override setUp method to disable Vite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace Tests;
-
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+    }
 }
